c3ca11b1fa6ab03b7665a1ca4d14462c486e446d
killkillsito <silverz6996@gmail.com>
2025-04-18 18:25:49 +0200

    Delete html directory

--- html/components/playerinformation/index.js
+++
@@ -1,8 +0,0 @@
-import importTemplate from '../../util/importTemplate.js';
-export default {
-    template: await importTemplate('components/playerinformation/index.html'),
-    data: () => ({}),
-    props : ["playerName", "playerJob", "playerPP", "theme"],
-    mounted (){
-    },
-}
